97a05d486f5757802f0b4f55e74ae739e7de045f
N K <konancys@gmail.com>
2014-11-13 13:37:56 -0500

    added playgame route

--- main.rb
+++ main.rb
@@ -4,7 +4,23 @@ require 'sinatra/reloader' if development?
 class RockPaperScissorGame < Sinatra::Base 
 	
 	get '/' do
-		"Hello World!"
+		erb:index
 	end
 
+	get '/playgame' do
+		
+		def play(choice)
+			@user_choice = params[:user_choice]
+			computer_choice = ["rock", "paper", "scissors"].sample(1)				
+			
+		end
+	
+	end
 end
+
+
+
+# Rock beats scissors 
+# paper beats rock 
+# Scissors beats paper 
+
