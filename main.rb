7aef5aa0e3a2339f7a30732326a4c38bcb291404
N K <konancys@gmail.com>
2014-11-16 23:28:25 -0500

    gems, sinatra, sinatra-contrib

--- main.rb
+++ main.rb
@@ -1,19 +1,27 @@
 require 'sinatra'
-require 'sinatra/reloader' 
+require 'sinatra/contrib/all' 
 	
-	get '/' do
-		erb:index
-	end
+get '/' do
+	erb:index
+end
 
-	get '/playgame' do
-		erb :playgame
+get '/:choice' do
+	
+	options = ["Rock", "Paper", "Scissors"]
+	user_choice = params[:choice].capitalize
+	computer_choice = options.sample(1)
+
+	if user_choice == computer_choice 
+		@move = "Computer chose " + computer_choice + ". It's a draw."
+	elsif 
+
+	
 	end
+	erb :playgame
+
+end
 		
-	
-
-
-
 # Rock beats scissors 
-# paper beats rock 
+# Paper beats rock 
 # Scissors beats paper 
 
